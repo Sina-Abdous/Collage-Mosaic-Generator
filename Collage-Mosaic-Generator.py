d4e8f24cae30931689f2421312aed6c5a778460f
Sina-Abdous <sina.abdoos3@gmail.com>
2020-04-13 15:34:24 +0430

    definig constants

--- Collage-Mosaic-Generator.py
+++ Collage-Mosaic-Generator.py
@@ -27,3 +27,31 @@ class TileProcessor:
 			w_crop = (w - min_dimension) / 2
 			h_crop = (h - min_dimension) / 2
 			img = img.crop((w_crop, h_crop, w - w_crop, h - h_crop))
+
+			large_tile_img = img.resize((TILE_SIZE, TILE_SIZE), Image.ANTIALIAS)
+			small_tile_img = img.resize((int(TILE_SIZE/TILE_BLOCK_SIZE), int(TILE_SIZE/TILE_BLOCK_SIZE)), Image.ANTIALIAS)
+
+			return (large_tile_img.convert('RGB'), small_tile_img.convert('RGB'))
+		except:
+			return (None, None)
+
+	def get_tiles(self):
+		large_tiles = []
+		small_tiles = []
+
+		print('Reading tiles from {}...'.format(self.tiles_directory))
+
+		# search the tiles directory recursively
+		for root, subFolders, files in os.walk(self.tiles_directory):
+			for tile_name in files:
+				print('Reading {:40.40}'.format(tile_name), flush=True, end='\r')
+				tile_path = os.path.join(root, tile_name)
+				large_tile, small_tile = self.__process_tile(tile_path)
+				if large_tile:
+					large_tiles.append(large_tile)
+					small_tiles.append(small_tile)
+
+
+		print('Processed {} tiles.'.format(len(large_tiles)))
+
+		return (large_tiles, small_tiles)
